5fb6593e0dd43785727d4ab8c2a993e1ead8cd49
eym3n <aymenzitouni2001@gmail.com>
2024-07-10 23:51:02 +0100

    refactor: cards

--- lib/widget/task_card.dart
+++ lib/widget/task_card.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
 import 'package:notes_app/model/task.dart';
 import 'package:intl/intl.dart';
+import 'package:notes_app/pages/task_editor.dart';
+import 'package:notes_app/utils/redux.dart';
+import 'package:redux/redux.dart';
+import 'package:tuple/tuple.dart';
 
 class TaskCard extends StatefulWidget {
   final Task task;
@@ -20,68 +25,217 @@ class _TaskCardState extends State<TaskCard> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        decoration: BoxDecoration(
-          color: widget.task.color,
-          borderRadius: BorderRadius.circular(20.0),
-          border: Border.all(
-            color: widget.task.color.withOpacity(0.5),
-            width: 1.0,
-          ),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.white.withOpacity(0.5),
-              blurRadius: 20.0,
-              spreadRadius: 20.0,
-              offset: const Offset(0, -20),
-            )
-          ],
-        ),
-        height: 150,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(
-                width: 200,
-                height: double.infinity,
-                child: Text(
-                  widget.task.title,
-                  softWrap: true,
-                  overflow: TextOverflow.fade,
-                  style: TextStyle(
-                      color: titleColor,
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.w600),
-                ),
-              ),
-              Column(
+    return StoreConnector<AppState,
+            Tuple2<void Function(String), void Function(String, Task)>>(
+        converter: (Store<AppState> store) {
+      return Tuple2(
+        (taskId) => store.dispatch(DeleteTaskAction(taskId)),
+        (taskId, updatedTask) =>
+            store.dispatch(UpdateTaskAction(taskId, updatedTask)),
+      );
+    }, builder: (context, actions) {
+      final deleteTask = actions.item1;
+      final updateTask = actions.item2;
+      return InkWell(
+        splashColor: Colors.transparent,
+        highlightColor: Colors.transparent,
+        onDoubleTap: () {
+          widget.task.completed = !widget.task.completed;
+          updateTask(widget.task.id, widget.task);
+        },
+        onLongPress: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (context) => Container(
+              height: 140,
+              margin: const EdgeInsets.symmetric(vertical: 20),
+              width: double.infinity,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                  const SizedBox(height: 13.0),
-                  Icon(
-                    !widget.task.completed
-                        ? CupertinoIcons.time_solid
-                        : CupertinoIcons.check_mark_circled_solid,
-                    color: iconColor.withOpacity(0.8),
-                    size: 30.0,
-                  ),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                  Flexible(
-                    child: Text(
-                        DateFormat.jm().format(widget.task.date).toString(),
-                        style: TextStyle(
-                            color: Colors.grey.shade600.withOpacity(0.8),
-                            fontSize: 12.0,
-                            fontWeight: FontWeight.w600)),
+                  (widget.task.completed
+                      ? InkWell(
+                          onTap: () {
+                            widget.task.completed = false;
+                            updateTask(widget.task.id, widget.task);
+                            Navigator.pop(context);
+                          },
+                          child: Container(
+                            margin: const EdgeInsets.symmetric(horizontal: 50),
+                            height: 70,
+                            width: double.infinity,
+                            child: const Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  'Mark as undone',
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                                Icon(
+                                  CupertinoIcons.check_mark_circled,
+                                  size: 20,
+                                  color: Colors.black,
+                                )
+                              ],
+                            ),
+                          ),
+                        )
+                      : InkWell(
+                          onTap: () {
+                            widget.task.completed = true;
+                            updateTask(widget.task.id, widget.task);
+                            Navigator.pop(context);
+                          },
+                          child: Container(
+                            margin: const EdgeInsets.symmetric(horizontal: 50),
+                            height: 70,
+                            width: double.infinity,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  'Mark as done',
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      color: Colors.green[800],
+                                      fontWeight: FontWeight.w600),
+                                ),
+                                Icon(
+                                  CupertinoIcons.check_mark_circled,
+                                  size: 20,
+                                  color: Colors.green[800],
+                                )
+                              ],
+                            ),
+                          ),
+                        )),
+                  InkWell(
+                    onTap: () {
+                      widget.task.completed = true;
+                      deleteTask(widget.task.id);
+                      Navigator.pop(context);
+                    },
+                    child: Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 50),
+                      height: 70,
+                      width: double.infinity,
+                      child: const Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Delete task',
+                            style: TextStyle(
+                                fontSize: 15,
+                                color: Colors.red,
+                                fontWeight: FontWeight.w600),
+                          ),
+                          Icon(
+                            CupertinoIcons.delete,
+                            size: 20,
+                            color: Colors.red,
+                          )
+                        ],
+                      ),
+                    ),
                   ),
                 ],
-              )
-            ],
-          ),
-        ));
+              ),
+            ),
+          );
+        },
+        onTap: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return TaskEditor(
+              task: widget.task,
+            );
+          }));
+        },
+        child: Container(
+            decoration: BoxDecoration(
+              color: widget.task.color,
+              borderRadius: BorderRadius.circular(20.0),
+              border: Border.all(
+                color: widget.task.color.withOpacity(0.5),
+                width: 1.0,
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.white.withOpacity(0.5),
+                  blurRadius: 20.0,
+                  spreadRadius: 20.0,
+                  offset: const Offset(0, -20),
+                )
+              ],
+            ),
+            height: 150,
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: 200,
+                    height: double.infinity,
+                    child: Text(
+                      widget.task.title,
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                      style: TextStyle(
+                          color: titleColor,
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      const SizedBox(height: 13.0),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: widget.task.completed
+                              ? Colors.green.shade700
+                              : widget.task.color,
+                          borderRadius: BorderRadius.circular(20.0),
+                          boxShadow: [
+                            BoxShadow(
+                              color: widget.task.color.withOpacity(0.8),
+                              blurRadius: 10.0,
+                              spreadRadius: 10.0,
+                              offset: const Offset(0.5, 0.8),
+                            )
+                          ],
+                        ),
+                        child: Icon(
+                          !widget.task.completed
+                              ? CupertinoIcons.time_solid
+                              : CupertinoIcons.check_mark_circled_solid,
+                          color: !widget.task.completed
+                              ? Colors.black.withOpacity(0.8)
+                              : Colors.white,
+                          size: 30.0,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      Flexible(
+                        child: Text(
+                            DateFormat.jm().format(widget.task.date).toString(),
+                            style: TextStyle(
+                                color: Colors.grey.shade600.withOpacity(0.8),
+                                fontSize: 12.0,
+                                fontWeight: FontWeight.w600)),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            )),
+      );
+    });
   }
 }
