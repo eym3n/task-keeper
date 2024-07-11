0e43ad12f438df670da00af6d6c5b01296b5669e
eym3n <aymenzitouni2001@gmail.com>
2024-07-11 18:53:45 +0100

    feat: time indicator + relevance sort

--- lib/utils/reducers.dart
+++ lib/utils/reducers.dart
@@ -1,20 +1,23 @@
 import 'package:notes_app/db/sqflite.dart';
+import 'package:notes_app/utils/functions.dart';
 import 'package:notes_app/utils/redux.dart';
 
 AppState tasksReducer(AppState state, dynamic action) {
   if (action is AddTaskAction) {
     Db.insertTask(action.task.toMap());
-    return AppState(tasks: List.from(state.tasks)..add(action.task));
+    return AppState(
+        tasks: sortByRelevance(List.from(state.tasks)..add(action.task)));
   } else if (action is UpdateTaskAction) {
     return AppState(
-        tasks: state.tasks.map((task) {
+        tasks: sortByRelevance(state.tasks.map((task) {
       Db.updateTask(task.toMap());
       return task.id == action.id ? action.updatedTask : task;
-    }).toList());
+    }).toList()));
   } else if (action is DeleteTaskAction) {
     Db.deleteTask(action.id);
     return AppState(
-        tasks: state.tasks.where((task) => task.id != action.id).toList());
+        tasks: sortByRelevance(
+            state.tasks.where((task) => task.id != action.id).toList()));
   } else if (action is InitializeTasksAction) {
     return AppState(tasks: action.tasks);
   }
